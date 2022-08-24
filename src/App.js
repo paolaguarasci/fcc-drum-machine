d0bd85124ed54059062381c1ed090d0a0a501977
Paola Guarasci <paolaguarasci@gmail.com>
2022-08-24 08:07:29 +0200

    clean

--- src/App.js
+++ src/App.js
@@ -1,14 +1,6 @@
 import './App.css';
 
 import s1 from './assets/sounds/drum/80s-Bdrum1.wav';
-import s10 from './assets/sounds/drum/80s-CRASH2.wav';
-import s11 from './assets/sounds/drum/80s-LOWCONGA.wav';
-import s12 from './assets/sounds/drum/80s-TOM3.wav';
-import s13 from './assets/sounds/drum/80s-CRASH3.wav';
-import s14 from './assets/sounds/drum/80s-MIDCONGA.wav';
-import s15 from './assets/sounds/drum/80s-TOM5.wav';
-import s16 from './assets/sounds/drum/80s-HHCLOSE1.wav';
-import s17 from './assets/sounds/drum/80s-SNARE1.wav';
 import s2 from './assets/sounds/drum/80s-HHCLOSE2.wav';
 import s3 from './assets/sounds/drum/80s-TAMB1.wav';
 import s4 from './assets/sounds/drum/80s-COWBELL1.wav';
